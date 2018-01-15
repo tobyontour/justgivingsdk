2d30bded3e0bfd49393602a68dddf2f020e79cbf
Toby Bettridge <toby.bettridge@gosh.org>
2018-01-15 16:56:28 +0000

    Account creation endpoint.

--- Tests/AccountServiceTest.php
+++ Tests/AccountServiceTest.php
@@ -4,6 +4,7 @@ namespace JustGivingApi\Tests;
 
 use PHPUnit\Framework\TestCase;
 use JustGivingApi\JustGivingApi;
+use JustGivingApi\Models\Account;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Middleware;
@@ -107,5 +108,64 @@ class AccountServiceTest extends TestCase
         );
 
         $this->assertTrue($result, 'Account does exist.');
+    }
+
+    public function testAccountRegistration()
+    {
+        $api = new JustGivingApi('API_KEY', null, true);
+
+        $handlerStack = $this->getMockHandlerStack($container, [
+            new Response(200, [], '{"accountType":"None"}') // Need actual content from a call.
+        ]);
+
+        $api->setHandlerStack($handlerStack);
+
+        $accountsService = $api->getAccountsService();
+
+        $this->assertEquals(
+            'JustGivingApi\Services\AccountsService',
+            get_class($accountsService)
+        );
+
+        $accountArray = [
+            "reference" => "myref",
+            "title" => "Mr",
+            "firstName" => "Bob",
+            "lastName" => "Dobalina",
+            "Address.line1" => "22 Acacia Avenue",
+            "Address.line2" => "Little Village",
+            "Address.townOrCity" => "Sometown",
+            "Address.countyOrState" => "Acmetown",
+            "Address.country" => "United Kingdom",
+            "Address.postcodeOrZipcode" => "AB12 CDE",
+            "email" => "bob.dobalina@example.com",
+            "password" => "PassWord",
+            "acceptTermsAndConditions" => true,
+            "causeId" => 123
+        ];
+
+        $account = new Account($accountArray);
+
+        $result = $accountsService->accountCreate($account);
+
+        $uri = $container[0]['request']->getUri();
+
+        $this->assertEquals(
+            JustGivingApi::SANDBOX_BASE_URL . '/API_KEY/v1/account',
+            (string)$uri
+        );
+
+        $this->assertEquals(
+            'PUT',
+            $container[0]['request']->getMethod()
+        );
+
+        $body = json_decode($container[0]['request']->getBody(), true);
+
+        $this->assertEquals(
+            0,
+            count(array_diff_assoc($body, $accountArray)),
+            'The array sent to the register user endpoint is the same one we defined.'
+        );
     }
 }
