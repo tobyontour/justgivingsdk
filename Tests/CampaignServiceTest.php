ed6d5b3b66fa8b2e61cf255c6ff30c977e680771
Toby Bettridge <Toby.Bettridge@GOSH.ORG>
2018-05-03 11:51:05 +0100

    CreateCampaign endpoint.

--- Tests/CampaignServiceTest.php
+++ Tests/CampaignServiceTest.php
@@ -2,6 +2,9 @@
 
 namespace JustGivingApi\Tests;
 
+use JustGivingApi\Models\Campaign;
+use JustGivingApi\Services\CampaignsService;
+
 use PHPUnit\Framework\TestCase;
 use JustGivingApi\JustGivingApi;
 use JustGivingApi\Models\Account;
@@ -71,5 +74,73 @@ class CampaignServiceTest extends TestCase
         $this->assertEquals('Gordon and Tana Ramsay Foundation', $results[1]->campaignPageName);
         $this->assertEquals(true, $results[1]->fundraisingEnabled);
         $this->assertEquals(2, count($results[1]->images));
+    }
+
+    public function testCreateCampaign()
+    {
+        $api = new JustGivingApi('API_KEY', null, false);
+
+        $handlerStack = $this->getMockHandlerStack($container, [
+            new Response(200, [], '{
+  "charityShortName": "short name",
+  "campaignShortUrl": "short url",
+  "campaignFullUrl": "full url",
+  "errorMessage": "string"
+}') // Need actual content from a call.
+        ]);
+
+        $api->setHandlerStack($handlerStack);
+
+        $campaignArray = array(
+            'campaignDeadline' => '2019-05-03T10:06:58.000Z',
+            'campaignThankYouMessage' => 'Thank you for the music',
+            'campaignPageName' => 'Race For The Santa 2018',
+            'campaignUrl' => 'http://campaign.staging.justgiving.com/charity/greatormondstreet/rfts',
+            'currency' => 'GBP',
+            'target' => '1000000',
+            'fundraisingEnabled' => true,
+            'story' => 'This is the story all about how my life got flipped turned upside down.',
+        );
+        $campaign = new Campaign($campaignArray);
+
+        $result = $api->getCampaignsService()->createCampaign($campaign);
+
+        $uri = $container[0]['request']->getUri();
+
+        $this->assertEquals(
+            JustGivingApi::SANDBOX_BASE_URL . '/API_KEY/v1/campaigns',
+            (string)$uri
+        );
+
+        $this->assertEquals(
+            'PUT',
+            $container[0]['request']->getMethod()
+        );
+
+        $body = json_decode($container[0]['request']->getBody(), true);
+
+        $expectedBody = array (
+          'campaignDeadline' => '2019-05-03T10:06:58.000Z',
+          'campaignThankYouMessage' => 'Thank you for the music',
+          'campaignName' => 'Race For The Santa 2018',
+          'campaignUrl' => 'http://campaign.staging.justgiving.com/charity/greatormondstreet/rfts',
+          'currencyCode' => 'GBP',
+          'campaignTarget' => '1000000',
+          'campaignStory' => 'This is the story all about how my life got flipped turned upside down.',
+          'fundraisingEnabled' => true,
+          'campaignSummary' => 'Summary'
+        );
+
+        $this->assertEquals(
+            0,
+            count(array_diff_assoc($body, $expectedBody)),
+            'The array sent to the register user endpoint is the same one we defined. ' .
+              print_r(array_diff_assoc($expectedBody, $body), true)
+        );
+
+        $this->assertTrue(isset($result->charityShortName), 'Return contains charityShortName');
+        $this->assertTrue(isset($result->campaignShortUrl), 'Return contains campaignShortUrl');
+        $this->assertTrue(isset($result->campaignFullUrl), 'Return contains campaignFullUrl');
+        $this->assertTrue(isset($result->errorMessage), 'Return contains errorMessage');
     }
 }
